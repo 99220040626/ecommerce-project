c477aef559597cb44dc1720423c3b712ca892bc6
Manyam Siva Santhosh Kumar Reddy <sivasanthoshmanyam48@gmail.com>
2026-03-04 16:41:58 +0530

    updated db.js to use internet database

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
+require("dotenv").config(); // This loads hidden internet settings
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root123",
-  database: "ecommerce_db"
-});
+// If Render gives it an internet link (DATABASE_URL), use it.
+// If not, use your local computer details!
+const db = process.env.DATABASE_URL 
+  ? mysql.createConnection(process.env.DATABASE_URL) 
+  : mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "root123",
+      database: "ecommerce_db"
+    });
 
 db.connect((err) => {
   if (err) {
